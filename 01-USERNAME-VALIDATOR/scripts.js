f73b2d8430523c5243517ca9e4c02eba874b7bd1
abdiweli ahmed <aabukar027@gmail.com>
2026-07-20 22:32:26 +0300

    Added uppercase restriction to username validator

--- 01-USERNAME-VALIDATOR/scripts.js
+++ 01-USERNAME-VALIDATOR/scripts.js
@@ -9,14 +9,14 @@ function ValidateUsername(){
   /* CHECKING EMPTY USERNAME*/
   if (username === "") {
     display.textContent = "please enter username";
-    display.style.color = "red"; 
+    display.style.color = "red";
     return;
   }
 
   /* CHECKING SPACES*/
   if (username.includes(" ")) {
     display.textContent = "username should not contain spaces";
-    display.style.color = "red"; 
+    display.style.color = "red";
 
     return;
   }
@@ -24,21 +24,28 @@ function ValidateUsername(){
   /* CHECKING USERNAME LENGTH*/
   if (username.length < 6) {
     display.textContent = "Username must be at least 6 characters";
-    display.style.color = "red"; 
+    display.style.color = "red";
     return;
   }
 
   /* CHECKING IF STARTS A LETTER */
-  let firstChar = username.charAt(0)
+  let firstChar = username.charAt(0);
 
-  if(firstChar >="0" && firstChar <= "9"){
+  if (firstChar >= "0" && firstChar <= "9") {
     display.textContent = "Username should start with a letter";
-   display.style.color = "red";
+    display.style.color = "red";
     return;
+  }
 
+  /* CHECKING UPPERCASE LETTERS */
+
+  if(username !==username.toLowerCase()){
+    display.textContent = "username should not contain uppercase letter";
+    display.style.color = "red";
+    return;
   }
   display.textContent = "Valid Username";
-   display.style.color = "green";
+  display.style.color = "green";
 }
 
 UsernameInput.addEventListener("input", ValidateUsername)

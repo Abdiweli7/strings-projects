9c2b3129cba2d28f4b5e5236d423877cc389033c
abdiweli ahmed <aabukar027@gmail.com>
2026-07-20 22:12:27 +0300

    add color validation message

--- 01-USERNAME-VALIDATOR/scripts.js
+++ 01-USERNAME-VALIDATOR/scripts.js
@@ -9,18 +9,22 @@ function ValidateUsername(){
   /* CHECKING EMPTY USERNAME*/
   if (username === "") {
     display.textContent = "please enter username";
+    display.style.color = "red"; 
     return;
   }
 
   /* CHECKING SPACES*/
   if (username.includes(" ")) {
     display.textContent = "username should not contain spaces";
+    display.style.color = "red"; 
+
     return;
   }
 
   /* CHECKING USERNAME LENGTH*/
   if (username.length < 6) {
     display.textContent = "Username must be at least 6 characters";
+    display.style.color = "red"; 
     return;
   }
 
@@ -29,10 +33,12 @@ function ValidateUsername(){
 
   if(firstChar >="0" && firstChar <= "9"){
     display.textContent = "Username should start with a letter";
+   display.style.color = "red";
     return;
 
   }
-  display.textContent = "Valid Username"
+  display.textContent = "Valid Username";
+   display.style.color = "green";
 }
 
 buttonElem.addEventListener("click", ValidateUsername)
